25790058ff009434f9cc02559d69c8278a54b36b
Matt V <MVV90@users.noreply.github.com>
2025-03-25 15:10:58 -0400

    revert - wlm cannot be setup during connection

--- lib/sequel/adapters/shared/redshift.rb
+++ lib/sequel/adapters/shared/redshift.rb
@@ -36,68 +36,6 @@ module Sequel
           sqls << "SET search_path = #{search_path.map{|s| "\"#{s.gsub('"', '""')}\""}.join(',')}"
         end
 
-        # Redshift Serverless options
-        # https://docs.aws.amazon.com/redshift/latest/dg/cm-c-wlm-query-monitoring-rules.html#cm-c-wlm-query-monitoring-metrics-serverless
-        # TODO: Implement the rest of these, but possibly skip checking 'valid values' on some of these? Some of these values look incorrect from AWS docs (example: # Valid values are 0–86,399)
-
-        # max_query_cpu_time
-        # Query CPU time. CPU time used by the query, in seconds. CPU time is distinct from Query execution time.
-        # Valid values are 0–999,999.
-
-        # max_query_blocks_read
-        # Blocks read. Number of 1 MB data blocks read by the query.
-        # Valid values are 0–1,048,575.
-
-        # max_scan_row_count
-        # Scan row count. The number of rows in a scan step. The row count is the total number of rows emitted before filtering rows marked for deletion (ghost rows) and before applying user-defined query filters.
-        # Valid values are 0–999,999,999,999,999.
-
-        # max_query_execution_time
-        # Query execution time. Elapsed execution time for a query, in seconds. Execution time doesn't include time spent waiting in a queue. If a query exceeds the set execution time, Amazon Redshift Serverless stops the query.
-        # Valid values are 0–86,399.
-        if max_query_execution_time = opts[:max_query_execution_time]
-          case max_query_execution_time
-          when String
-            # good
-          when Integer
-            # good
-          else
-            raise Error, "unrecognized type for :max_query_execution_time. Expected type Integer or String, but instead got: #{max_query_execution_time.inspect}"
-          end
-          sqls << "SET max_query_execution_time = '#{max_query_execution_time}'"
-        end
-
-        # max_query_queue_time
-        # Query queue time. Time spent waiting in a queue, in seconds.
-        # Valid values are 0–86,399.
-        if max_query_queue_time = opts[:max_query_queue_time]
-          case max_query_queue_time
-          when String
-            # good
-          when Integer
-            # good
-          else
-            raise Error, "unrecognized type for :max_query_queue_time. Expected type Integer or String, but instead got: #{max_query_queue_time.inspect}"
-          end
-          sqls << "SET max_query_queue_time = '#{max_query_queue_time}'"
-        end
-
-        # max_query_cpu_usage_percent
-        # CPU usage. Percent of CPU capacity used by the query.
-        # Valid values are 0–6,399.
-
-        # max_query_temp_blocks_to_disk
-        # Memory to disk. Temporary disk space used to write intermediate results, in 1 MB blocks.
-        # Valid values are 0–319,815,679.
-
-        # max_join_row_count
-        # Rows joined. The number of rows processed in a join step.
-        # Valid values are 0–999,999,999,999,999.
-
-        # max_nested_loop_join_row_count
-        # Nested loop join row count. The number or rows in a nested loop join.
-        # Valid values are 0–999,999,999,999,999.
-
         sqls
       end
 
